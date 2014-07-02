6eed3b6d0367a69ffe5fc6dc3ea2d2d2f71bbd5b
Steven Haryanto (on PC, Jakarta) <stevenharyanto@gmail.com>
2014-07-02 15:34:29 +0700

    Initial commit

---
+++ lib/Complete/Zsh.pm
@@ -0,0 +1,21 @@
+package Complete::Zsh;
+
+use 5.010001;
+
+# DATE
+# VERSION
+
+1;
+#ABSTRACT: Completion module for the Z shell
+
+=head1 DESCRIPTION
+
+Not yet implemented. Land grab :)
+
+
+=head1 SEE ALSO
+
+As far as I know, at the time of this writing, there's currently no other CPAN
+modules related to zsh completion.
+
+=cut
