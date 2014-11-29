7bb55fcfc52376b2fe71e7396d90d5e286cbd158
perlancar (@pc-home) <perlancar@gmail.com>
2014-11-29 17:23:33 +0700

    Implement first version

--- lib/Complete/Zsh.pm
+++ lib/Complete/Zsh.pm
@@ -1,21 +1,116 @@
 package Complete::Zsh;
 
-use 5.010001;
-
 # DATE
 # VERSION
 
+use 5.010001;
+use strict;
+use warnings;
+
+require Exporter;
+our @ISA = qw(Exporter);
+our @EXPORT_OK = qw(
+                       parse_cmdline
+                       format_completion
+               );
+
+require Complete::Bash;
+
+our %SPEC;
+
+$SPEC{parse_cmdline} = {
+    v => 1.1,
+    summary => 'Parse shell command-line for processing by completion routines',
+    description => <<'_',
+
+This function converts COMP_LINE (str) (which can be supplied by zsh from `read
+-l`) and COMP_POINT (int) (which can be supplied by zsh from `read -ln`) into
+COMP_WORDS (array) and COMP_CWORD (int), like what bash supplies to shell
+functions. Currently implemented using `Complete::Bash`'s `parse_cmdline`.
+
+_
+    args_as => 'array',
+    args => {
+        cmdline => {
+            summary => 'Command-line, defaults to COMP_LINE environment',
+            schema => 'str*',
+            pos => 0,
+        },
+    },
+    result => {
+        schema => ['array*', len=>2],
+        description => <<'_',
+
+Return a 2-element array: `[$words, $cword]`. `$words` is array of str,
+equivalent to `COMP_WORDS` provided by bash to shell functions. `$cword` is an
+integer, equivalent to `COMP_CWORD` provided by bash to shell functions. The
+word to be completed is at `$words->[$cword]`.
+
+Note that COMP_LINE includes the command name. If you want the command-line
+arguments only (like in `@ARGV`), you need to strip the first element from
+`$words` and reduce `$cword` by 1.
+
+_
+    },
+    result_naked => 1,
+};
+sub parse_cmdline {
+    my ($line) = @_;
+
+    $line //= $ENV{COMP_LINE};
+    Complete::Bash::parse_cmdline($line, length($line));
+}
+
+$SPEC{format_completion} = {
+    v => 1.1,
+    summary => 'Format completion for output (for shell)',
+    description => <<'_',
+
+zsh accepts completion reply in the form of one entry per line to STDOUT.
+Currently the formatting is done using `Complete::Bash`'s `format_completion`.
+
+_
+    args_as => 'array',
+    args => {
+        completion => {
+            summary => 'Completion answer structure',
+            description => <<'_',
+
+Either an array or hash, as described in `Complete`.
+
+_
+            schema=>['any*' => of => ['hash*', 'array*']],
+            req=>1,
+            pos=>0,
+        },
+    },
+    result => {
+        summary => 'Formatted string (or array, if `as` key is set to `array`)',
+        schema => ['any*' => of => ['str*', 'array*']],
+    },
+    result_naked => 1,
+};
+sub format_completion {
+    Complete::Bash::format_completion(@_);
+}
+
 1;
-#ABSTRACT: Completion module for the Z shell
+#ABSTRACT: Completion module for zsh shell
 
 =head1 DESCRIPTION
 
-Not yet implemented. Land grab :)
+This module provides routines related to doing completion in zsh.
+
+
+=head1 TODOS
 
 
 =head1 SEE ALSO
 
-As far as I know, at the time of this writing, there's currently no other CPAN
-modules related to zsh completion.
+L<Complete>
+
+L<Complete::Bash>, L<Complete::Fish>, L<Complete::Tcsh>.
+
+zshcompctl manual page.
 
 =cut
